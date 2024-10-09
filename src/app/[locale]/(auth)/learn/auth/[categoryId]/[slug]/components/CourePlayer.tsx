e5ea2c88d926bfc081e63575e19bbb62c0c079b4
ducnd <nguyendinhduc.github@gmail.com>
2024-10-10 00:44:01 +0700

    Refactor Header component to fix menu icon visibility issue

--- src/app/[locale]/(auth)/learn/auth/[categoryId]/[slug]/components/CourePlayer.tsx
+++ src/app/[locale]/(auth)/learn/auth/[categoryId]/[slug]/components/CourePlayer.tsx
@@ -1,8 +1,13 @@
 'use client';
+import '@/styles/course.css';
+import '@/styles/home.css';
+import '@/styles/root.css';
+
+import Link from 'next/link';
 import React from 'react';
 
-import PlyrPlayer from '@/components/Plyr';
-import getUrlImage from '@/libs/common';
+import getUrlImage, { getVideoUrl } from '@/libs/common';
+import { Button } from 'antd';
 
 type Props = {
   data: API.CourseMaterial;
@@ -15,13 +20,9 @@ type Props = {
 
 const CourePlayer = (props: Props) => {
   const datas = props.listData;
-  const detail = props.data;
-  console.log('CourePlayer', props);
+  // const detail = props.data;
 
-  // state detail
-  // const [detail, setDetail] = React.useState<API.CourseMaterial | null>(null);
-  // state detail
-  // const [detail, setDetail] = React.useState<API.CourseMaterial | null>(null);
+  const [detail, setDetail] = React.useState<API.CourseMaterial | null>(props.listData.data.courses[0]?.course_materials[0] || null);
   const getTotalTime = (course: Model.Course) => {
     let total = 0;
     course.course_materials.forEach((material) => {
@@ -35,22 +36,40 @@ const CourePlayer = (props: Props) => {
 
     return `${hours}:${minutes}:${seconds}`;
   };
-
   return (
     <div className="mx-auto flex max-w-[1600px] py-8">
-      <div className="mr-8 aspect-video h-[60vh] w-3/5">
-        <PlyrPlayer url="http://localhost:8000/api/v1/file/11/stream" />
-        {/* <iframe
-          src="http://localhost:8000/api/v1/file/11/stream"
-          title={detail?.title}
-          className="size-full"
-          frameBorder="0"
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-          sandbox="allow-scripts allow-same-origin allow-popups"
-          onContextMenu={(e: any) => e.preventDefault()}
-        >
-        </iframe> */}
+      <div className="relative mr-8 aspect-video h-[60vh] w-3/5 rounded-xl">
+        {/* <PlyrPlayer url="http://localhost:8000/api/v1/file/11/stream" /> */}
+
+        {!detail?.is_free
+          ? (
+            <div  className="flex items-center justify-center w-full h-full rounded-xl bg-slate-400  bg-opacity-10 ">
+                <Link href="/learn/payment" className='text-white'>
+                  <div className="header-bot-item" data-attribute="register">
+                      {' '}
+                      <div className="btn-header btn-header-js bg-lms">
+                        <span className="txt fw-600">
+                          Nâng cấp ngay
+                        </span>
+                      </div>
+                    </div>
+                </Link>
+              </div>
+            )
+          : (
+              <iframe
+                src={`/video/${detail?.id}`}
+                title={detail?.title}
+                className="size-full rounded-lg"
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen
+                sandbox="allow-scripts allow-same-origin allow-popups"
+                onContextMenu={(e: any) => e.preventDefault()}
+                style={{ width: '100%', height: '100%' }}
+              >
+              </iframe>
+            )}
         <div>
           <div className="t-title-second c-second mt-8 !text-black">{detail?.title}</div>
           <div className="flex">
@@ -141,7 +160,18 @@ const CourePlayer = (props: Props) => {
                   <div className="pro-box-body-inner">
                     <div className="pro-list row">
                       {data?.course_materials?.map((video, index) => (
-                        <div className="pro-item w-full" key={index}>
+                        <div
+                          className="w-full cursor-pointer"
+                          key={video.id}
+                          role="button"
+                          tabIndex={0}
+                          onClick={() => setDetail(video)}
+                          onKeyPress={(e) => {
+                            if (e.key === 'Enter' || e.key === ' ') {
+                              setDetail(video);
+                            }
+                          }}
+                        >
                           <div className="pro-wr flex rounded-md p-4">
                             <div className="pro-img mr-4">
                               <div className="pro-stt">
--- src/libs/common.ts
+++ src/libs/common.ts
@@ -8,3 +8,15 @@ export default function getUrlImage(url: string | null | undefined) {
     return process.env.NEXT_PUBLIC_MEDIA_URL + url;
   }
 }
+
+export function getSlug(title: string) {
+  return title.toLowerCase().replace(/ /g, '-');
+}
+
+export function getVideoUrl(url: string) {
+  if (url?.startsWith('http')) {
+    return url;
+  } else {
+    return process.env.NEXT_PUBLIC_MEDIA_URL + url;
+  }
+}
