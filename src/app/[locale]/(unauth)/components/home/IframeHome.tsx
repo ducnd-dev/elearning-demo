e27d05a51c53839160cb269235dc882d14dc85cf
huongtx <phamhyta@gmail.com>
2024-10-19 18:25:15 +0700

    Refactor IframeHome component to set a fixed height for the iframe

--- src/app/[locale]/(unauth)/components/home/IframeHome.tsx
+++ src/app/[locale]/(unauth)/components/home/IframeHome.tsx
@@ -1,33 +1,33 @@
-// 'use client';
-// import { useEffect, useState } from 'react'
+'use client';
+import { useEffect, useState } from 'react'
 
 // type Props = {
 //   height: string;
 // }
 const IframeHome = () => {
-  // const [iframeHeight, setIframeHeight] = useState('600vh');
+  const [iframeHeight, setIframeHeight] = useState('2794vh');
 
-  // useEffect(() => {
-  //   const iframe = document.getElementById('iframe-home') as HTMLIFrameElement;
-  //   if (iframe) {
-  //     const updateHeight = () => {
-  //       setIframeHeight(`${iframe?.contentWindow?.document.body.scrollHeight}px`);
-  //     };
-  //     // console.log(iframe?.contentWindow?.document.body.scrollHeight);
-  //     iframe.addEventListener('load', updateHeight);
-  //     window.addEventListener('resize', updateHeight);
+  useEffect(() => {
+    const iframe = document.getElementById('iframe-home') as HTMLIFrameElement;
+    if (iframe) {
+      const updateHeight = () => {
+        setIframeHeight('2794vh');
+      };
+      console.log(iframe?.contentWindow?.document.body.scrollHeight);
+      iframe.addEventListener('load', updateHeight);
+      window.addEventListener('resize', updateHeight);
 
-  //     return () => {
-  //       iframe.removeEventListener('load', updateHeight);
-  //       window.removeEventListener('resize', updateHeight);
-  //     };
-  //   }
-  // }, []);
+      return () => {
+        iframe.removeEventListener('load', updateHeight);
+        window.removeEventListener('resize', updateHeight);
+      };
+    }
+  }, []);
   return (
     <iframe 
     id='iframe-home' 
     src="https://builderjs.cuongdesign.net/templates/default/ColleagueCourses" 
-    style={{ height: "2794vh", width: '100%', border: 'none', overflow: 'hidden' }}
+    style={{ height: iframeHeight, width: '100%', border: 'none', overflow: 'hidden' }}
     ></iframe>
   )
 }
