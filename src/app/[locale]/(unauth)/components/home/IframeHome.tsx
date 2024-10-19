aaebd9b0c72eda77fbffcda77643c0d5d2e44016
huongtx <phamhyta@gmail.com>
2024-10-19 15:53:47 +0700

    Refactor IframeHome component to set a fixed height for the iframe

--- src/app/[locale]/(unauth)/components/home/IframeHome.tsx
+++ src/app/[locale]/(unauth)/components/home/IframeHome.tsx
@@ -2,15 +2,15 @@
 import { useEffect, useState } from 'react'
 
 const IframeHome = () => {
-  const [iframeHeight, setIframeHeight] = useState('100vh');
+  const [iframeHeight, setIframeHeight] = useState('600vh');
 
   useEffect(() => {
     const iframe = document.getElementById('iframe-home') as HTMLIFrameElement;
     if (iframe) {
       const updateHeight = () => {
-        setIframeHeight(`${iframe?.contentWindow?.document.body.scrollHeight}px`);
+        // setIframeHeight(`${iframe?.contentWindow?.document.body.scrollHeight}px`);
       };
-      console.log(iframe?.contentWindow?.document.body.scrollHeight);
+      // console.log(iframe?.contentWindow?.document.body.scrollHeight);
       iframe.addEventListener('load', updateHeight);
       window.addEventListener('resize', updateHeight);
 
