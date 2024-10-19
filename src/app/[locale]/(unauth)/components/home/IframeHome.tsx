f4e4f0196bf45abbba47f973a19b0287018a3936
ducnd <nguyendinhduc.github@gmail.com>
2024-10-19 16:58:44 +0700

    Refactor IframeHome component to set a fixed height for the iframe

--- src/app/[locale]/(unauth)/components/home/IframeHome.tsx
+++ src/app/[locale]/(unauth)/components/home/IframeHome.tsx
@@ -1,27 +1,27 @@
 'use client';
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 const IframeHome = () => {
-  const [iframeHeight, setIframeHeight] = useState('600vh');
+  // const [iframeHeight, setIframeHeight] = useState('600vh');
 
-  useEffect(() => {
-    const iframe = document.getElementById('iframe-home') as HTMLIFrameElement;
-    if (iframe) {
-      const updateHeight = () => {
-        // setIframeHeight(`${iframe?.contentWindow?.document.body.scrollHeight}px`);
-      };
-      // console.log(iframe?.contentWindow?.document.body.scrollHeight);
-      iframe.addEventListener('load', updateHeight);
-      window.addEventListener('resize', updateHeight);
+  // useEffect(() => {
+  //   const iframe = document.getElementById('iframe-home') as HTMLIFrameElement;
+  //   if (iframe) {
+  //     const updateHeight = () => {
+  //       setIframeHeight(`${iframe?.contentWindow?.document.body.scrollHeight}px`);
+  //     };
+  //     // console.log(iframe?.contentWindow?.document.body.scrollHeight);
+  //     iframe.addEventListener('load', updateHeight);
+  //     window.addEventListener('resize', updateHeight);
 
-      return () => {
-        iframe.removeEventListener('load', updateHeight);
-        window.removeEventListener('resize', updateHeight);
-      };
-    }
-  }, []);
+  //     return () => {
+  //       iframe.removeEventListener('load', updateHeight);
+  //       window.removeEventListener('resize', updateHeight);
+  //     };
+  //   }
+  // }, []);
   return (
-    <iframe id='iframe-home' src="https://builderjs.cuongdesign.net/templates/default/ColleagueCourses" style={{ width: '100%', height: iframeHeight, border: 'none', overflow: 'hidden' }}></iframe>
+    <iframe id='iframe-home' src="https://builderjs.cuongdesign.net/templates/default/ColleagueCourses" onLoad={(e) => { e.currentTarget.style.height = e.currentTarget.contentWindow?.document.body.scrollHeight + 'px'; }} style={{ height: '200px', width: '100%', border: 'none', overflow: 'hidden' }}></iframe>
   )
 }
 
