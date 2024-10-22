8dcd55d52819dced2cee47e2288e15886b98896b
yopaz-huongtx <huongtx@yopaz.vn>
2024-10-22 13:36:35 +0900

    Refactor IframeHome component to fetch and render HTML content dynamically

--- src/app/[locale]/(unauth)/components/home/IframeHome.tsx
+++ src/app/[locale]/(unauth)/components/home/IframeHome.tsx
@@ -1,9 +1,21 @@
+import { revalidatePath } from "next/cache";
+import Swiper from 'swiper';
+import { SwiperOptions } from 'swiper/types';
+
+const swiperParams: SwiperOptions = {
+  slidesPerView: 3,
+  spaceBetween: 50,
+};
+
+new Swiper('.swiper-container', swiperParams);
+
 const html = async () => {
-  const response = await fetch('https://builderjs.cuongdesign.net/templates/default/ColleagueCourses?ver=1.0.0');
+  const response = await fetch('https://builderjs.cuongdesign.net/templates/default/ColleagueCourses');
   const text = await response.text();
   return text;
 };
 const IframeHome = async () => {
+  revalidatePath('https://builderjs.cuongdesign.net/templates/default/ColleagueCourses');
   const data = await html();
   return (
     <div
