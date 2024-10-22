33b6d743f2b522520caeb9690f3c6409789d11c8
yopaz-huongtx <huongtx@yopaz.vn>
2024-10-22 12:57:41 +0900

    Refactor IframeHome component to fetch and render HTML content dynamically

--- src/app/[locale]/(unauth)/components/home/IframeHome.tsx
+++ src/app/[locale]/(unauth)/components/home/IframeHome.tsx
@@ -1,18 +1,18 @@
 const html = async () => {
-  const response = await fetch('https://builderjs.cuongdesign.net/templates/default/ColleagueCourses');
+  const response = await fetch('https://builderjs.cuongdesign.net/templates/default/ColleagueCourses?ver=1.0.0');
   const text = await response.text();
   return text;
-}
+};
 const IframeHome = async () => {
   const data = await html();
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html:  data
+        __html: data,
       }}
     >
     </div>
-  )
-}
+  );
+};
 
-export default IframeHome
+export default IframeHome;
