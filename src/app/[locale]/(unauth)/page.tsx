d32da48f132ef7c2bc7d33465a093ec9079c4e46
huongtx <phamhyta@gmail.com>
2024-10-26 15:44:50 +0700

    Refactor IframeHome component to remove unused Swiper library and update HTML content URL

--- src/app/[locale]/(unauth)/page.tsx
+++ src/app/[locale]/(unauth)/page.tsx
@@ -1,8 +1,4 @@
-import '@/styles/home.css';
-import '@/styles/root.css';
-
 import IframeHome from './components/home/IframeHome';
-import { FormLogin } from './components/login/FormLogin';
 
 export default function Index() {
   return (
@@ -18,13 +14,6 @@ export default function Index() {
       <Intro />
       <Staff />
       <Faqs /> */}
-      <div className='w-full hidden justify-end text-white'>
-        <div className='w-1/3 mr-28 relative h-0'>
-          <div className='absolute top-28 px-5'>
-            <FormLogin />
-          </div>
-        </div>
-      </div>
       <IframeHome />
     </>
   );
