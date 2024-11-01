56f1a94270a50d789bcce25f0b5ca37317821b23
yopaz-huongtx <huongtx@yopaz.vn>
2024-11-01 13:05:28 +0900

    Refactor UI styling and improve video playback in CourePlayer component

--- src/app/[locale]/(auth)/learn/payment/components/CountDown.tsx
+++ src/app/[locale]/(auth)/learn/payment/components/CountDown.tsx
@@ -4,21 +4,31 @@ import React from 'react'
 
 const CountDown = () => {
   const { Countdown } = Statistic;
+  const [count, setCount] = React.useState(Date.now() + 1000 * 60);
+  const [countM, setCountM] = React.useState(Date.now() + 1000 * 60 * 5);
+  const [mCount, setMCount] = React.useState(4);
 
   return (
     <div className="pay-cw">
       <div className="pay-cw-item">
         <p className="number">
-          <Countdown value={Date.now() + 1000 * 60 * 5} format="mm" valueStyle={{ color: '#fff' }}/>
+          <Countdown value={countM} format="mm" valueStyle={{ color: '#fff' }} />
         </p>
         <p className="des">Phút</p>
       </div>
       <div className="pay-cw-item">
         <p className="number">
           <Countdown 
-            value={Date.now() + 1000 * 60} 
+            value={count}
             format="ss" 
             valueStyle={{ color: 'red' }}
+            onFinish={() => {
+              if (mCount >= 0) {
+                setCount(count + 1000 * 60);
+                setMCount(mCount - 1);
+                setCountM(countM);
+              }
+            }}
           />
         </p>
         <p className="des">Giây</p>
