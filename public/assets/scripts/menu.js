66797a23eb8c128865e761652e8d77e14abb820d
ducnd <nguyendinhduc.github@gmail.com>
2024-11-17 21:18:20 +0700

    Refactor menu item click handlers to use window.location.pathname for URL checks

--- public/assets/scripts/menu.js
+++ public/assets/scripts/menu.js
@@ -2,8 +2,8 @@
 document.querySelectorAll('.scroll-quyen_loi').forEach(item => {
   item.addEventListener('click', event => {
     // get current url 
-    let currentUrl = window.location.href.split('?')[0];
-    if (!['/', '/en'].includes(currentUrl)) {
+    const currentPath = window.location.pathname;
+    if (!['/', '/en'].includes(currentPath)) {
       window.open(window.location.origin + '/#quyen_loi', '_self');
       return;
     }
@@ -15,8 +15,8 @@ document.querySelectorAll('.scroll-quyen_loi').forEach(item => {
 
 document.querySelectorAll('.scroll-chuong_trinh_hoc').forEach(item => {
   item.addEventListener('click', event => {
-    const currentUrl = window.location.href.split('?')[0];
-    if (!['/', '/en'].includes(currentUrl)) {
+    const currentPath = window.location.pathname;
+    if (!['/', '/en'].includes(currentPath)) {
       window.open(window.location.origin + '/#chuong_trinh_hoc', '_self');
       return;
     }
@@ -28,8 +28,8 @@ document.querySelectorAll('.scroll-chuong_trinh_hoc').forEach(item => {
 
 document.querySelectorAll('.scroll-la_ai').forEach(item => {
   item.addEventListener('click', event => {
-    const currentUrl = window.location.href.split('?')[0];
-    if (!['/', '/en'].includes(currentUrl)) {
+    const currentPath = window.location.pathname;
+    if (!['/', '/en'].includes(currentPath)) {
       window.open(window.location.origin + '/#la_ai', '_self');
       return;
     }
