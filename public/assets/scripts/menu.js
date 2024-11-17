5342a3d150029a489ba30144f050fdd85ac78bc9
ducnd <nguyendinhduc.github@gmail.com>
2024-11-17 20:52:53 +0700

    Enhance menu item click handlers to redirect with query parameters for non-root URLs

--- public/assets/scripts/menu.js
+++ public/assets/scripts/menu.js
@@ -1,5 +1,12 @@
+
 document.querySelectorAll('.scroll-quyen_loi').forEach(item => {
   item.addEventListener('click', event => {
+    // get current url 
+    let currentUrl = window.location.href.split('?')[0];
+    if (!['/', '/en'].includes(currentUrl)) {
+      window.open(currentUrl + '?id=quyen_loi', '_self');
+      return;
+    }
     document.getElementById('quyen_loi').scrollIntoView({
       behavior: 'smooth'
     });
@@ -8,6 +15,11 @@ document.querySelectorAll('.scroll-quyen_loi').forEach(item => {
 
 document.querySelectorAll('.scroll-chuong_trinh_hoc').forEach(item => {
   item.addEventListener('click', event => {
+    const currentUrl = window.location.href.split('?')[0];
+    if (!['/', '/en'].includes(currentUrl)) {
+      window.open(currentUrl + '?id=chuong_trinh_hoc', '_self');
+      return;
+    }
     document.getElementById('chuong_trinh_hoc').scrollIntoView({
       behavior: 'smooth'
     });
@@ -16,6 +28,11 @@ document.querySelectorAll('.scroll-chuong_trinh_hoc').forEach(item => {
 
 document.querySelectorAll('.scroll-la_ai').forEach(item => {
   item.addEventListener('click', event => {
+    const currentUrl = window.location.href.split('?')[0];
+    if (!['/', '/en'].includes(currentUrl)) {
+      window.open(currentUrl + '?id=la_ai', '_self');
+      return;
+    }
     document.getElementById('la_ai').scrollIntoView({
       behavior: 'smooth'
     });
